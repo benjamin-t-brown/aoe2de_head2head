446177bbbac02405adf29d301552fc3cffd1400a
Benjamin Brown <benjamin.t.brown@hotmail.com>
2020-06-19 01:20:26 -0600

    build lang

--- build.rs
+++ build.rs
@@ -5,7 +5,6 @@ use std::process::Command;
 // use std::path::Path;
 
 fn main() {
-  println!("cargo build-ui");
   Command::new("yarn").current_dir("./ui").status().unwrap();
   Command::new("yarn")
     .args(&["build"])
