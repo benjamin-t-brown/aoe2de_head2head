76305e55d52974c61dfdc9407adbe50fdd8ca75c
Benjamin Brown <benjamin.t.brown@hotmail.com>
2020-06-19 01:41:30 -0600

    add build, remove yarn from rust process

---
+++ ui/build/precache-manifest.610b95ef4d638c1b275c197655cbb725.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb67cdbcb8468cdca276c1e75b1745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee2b34f0b749e857ecb",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492b26c7121c6b4b9c21",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "a101c023279dd46e8185",
+    "url": "/static/js/runtime-main.js"
+  }
+]);
